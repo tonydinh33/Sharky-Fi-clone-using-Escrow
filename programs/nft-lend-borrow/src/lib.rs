42efe7dcc59289174cd4e72120a70d73e03aefd1
tonydinh33 <164116881+tonydinh33@users.noreply.github.com>
2024-03-20 09:55:14 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("TkrQQjKV28zyKjb8LhnjGn7uvc366pUpqPJa7tUYMDh");
 
 #[program]
 pub mod nft_lend_borrow {
